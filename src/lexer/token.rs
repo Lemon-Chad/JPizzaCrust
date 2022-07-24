dc1cbbcf4fcf98c194b98fabd881cf518f67d3c8
Lemon-Chad <jaolsm6296@gmail.com>
2022-07-24 19:34:59 -0400

    to_string for token and position

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 
 /// Stores the position of something in 
 /// a string (src).
@@ -25,6 +26,13 @@ impl Position {
     }
 }
 
+impl fmt::Display for Position {
+    // Writes position as {index:len} for to_string
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{}:{}}", index, len)
+    }
+}
+
 /// Tokens produced by the lexer.
 /// Every token stores at least a position.
 pub enum Token {
@@ -33,4 +41,11 @@ pub enum Token {
     Star(Position),
     Slash(Position),
     Number(Position, i32),
+}
+
+impl fmt::Display for Token {
+    // Simply writes token as debug for to_string
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
