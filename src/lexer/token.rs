cd5d3719e5b0d78f514e229b63de567617e6a30d
Lemon-Chad <jaolsm6296@gmail.com>
2022-07-24 20:03:53 -0400

    Lifetimes

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -5,13 +5,17 @@ use std::fmt;
 /// Index indicates the starting character's index,
 /// and len indicates how many characters
 /// the selection spans.
-pub struct Position {
+pub struct Position<'a> {
     index: u32,
     len: u32,
-    src: &str,
+    src: &'a str,
 }
 
-impl Position {
+impl<'a> Position<'a> {
+    pub fn new(index: u32, len: u32, src: &'a str) -> Self {
+        Position { index, len, src }
+    }
+
     // Some basic getters for each property.
     pub fn index(&self) -> u32 {
         self.index
@@ -26,26 +30,26 @@ impl Position {
     }
 }
 
-impl fmt::Display for Position {
+impl<'a> fmt::Display for Position<'a> {
     // Writes position as {index:len} for to_string
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{}:{}}", index, len)
+        write!(f, "{{{}:{}}}", self.index, self.len)
     }
 }
 
 /// Tokens produced by the lexer.
 /// Every token stores at least a position.
-pub enum Token {
-    Plus(Position),
-    Minus(Position),
-    Star(Position),
-    Slash(Position),
-    Number(Position, i32),
+pub enum Token<'a> {
+    Plus(Position<'a>),
+    Minus(Position<'a>),
+    Star(Position<'a>),
+    Slash(Position<'a>),
+    Number(Position<'a>, i32),
 }
 
-impl fmt::Display for Token {
+impl<'a> fmt::Display for Token<'a> {
     // Simply writes token as debug for to_string
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{}", self.to_string())
     }
 }
