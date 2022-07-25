43cdbe8d8d120a7b7447d51342ce2aba03107f59
Lemon-Chad <jaolsm6296@gmail.com>
2022-07-24 22:52:28 -0400

    Print lexed tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+mod lexer;
+mod utils;
 
 fn main() {
-    println!("Hello, world!");
+    let demo_code = "1 + 2 / 3 - 4 * 5";
+    let result = lexer::lexer::lex(demo_code);
+    if let lexer::result::LexResult::Ok(tokens) = result {
+        let mut s = "[ ".to_string();
+        for token in tokens {
+            let token_str = token.to_string();
+            s.push_str(&token_str);
+            s.push_str(", ");
+        }
+        s.pop();
+        s.pop();
+        s.push_str(" ]");
+        print!("{}", s);
+    }
 }
