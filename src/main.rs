ea213a799e569474812c656509d1f70ad155df0c
Lemon-Chad <jaolsm6296@gmail.com>
2022-07-25 01:08:22 -0400

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod lexer;
 mod utils;
+use utils::logging;
 
 fn main() {
-    let demo_code = "1 + 2 / 3 - 4 * 5";
-    let result = lexer::lexer::lex(demo_code);
+    let demo_code = "1. + 2 / 3 - 4 * 5";
+    let result = lexer::lexer::lex("demo_code".to_string(), demo_code);
+    logging::println(&format!("Demo code: {}", demo_code));
     if let lexer::result::LexResult::Ok(tokens) = result {
         let mut s = "[ ".to_string();
         for token in tokens {
@@ -14,6 +16,6 @@ fn main() {
         s.pop();
         s.pop();
         s.push_str(" ]");
-        print!("{}", s);
+        logging::println(&format!("Tokens: {}", s));
     }
 }
